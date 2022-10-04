f8dcd136edfbddccbdc3b336b420de018dee684e
Michelle Morry <michelle.morry@gmail.com>
2022-10-04 11:09:22 -0700

    remove button references

--- android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
+++ android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
@@ -8,25 +8,6 @@ import com.facebook.react.uimanager.ThemedReactContext
 class GooglePayButtonView(private val context: ThemedReactContext) : FrameLayout(context) {
   private var buttonType: String? = null
 
-  fun initialize() {
-    val type =
-      when (buttonType) {
-        "pay" -> R.layout.pay_with_googlepay_button_no_shadow
-        "pay_dark" -> R.layout.pay_with_googlepay_button_dark
-        "pay_shadow" -> R.layout.pay_with_googlepay_button
-        "standard" -> R.layout.googlepay_button_no_shadow
-        "standard_dark" -> R.layout.googlepay_button_dark
-        "standard_shadow" -> R.layout.googlepay_button
-        else -> if (isNightMode()) R.layout.googlepay_button_dark else R.layout.googlepay_button
-      }
-
-    val button = LayoutInflater.from(context).inflate(
-      type, null
-    )
-
-    addView(button)
-  }
-
   fun setType(type: String) {
     buttonType = type
   }
